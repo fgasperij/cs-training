630089de403fddeeb9a3e7fe918aa0d1240063d0
Fernando Gasperi Jabalera <fgasperijabalera@gmail.com>
2014-10-14 20:03:37 -0300

    WIP: Road construction

---
+++ codeforces/330b_road_construction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+	int n, m;
+	int is_restricted[1001];
+
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		
+	}
+	return 0;
+}
