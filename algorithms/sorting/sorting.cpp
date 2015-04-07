83334be83d671a01dea5d9046c4d0527b795be2d
Fernando Gasperi Jabalera <fjabalera@manas.com.ar>
2015-04-07 12:27:12 -0300

    Merge sort with optimizations.

--- algorithms/sorting/sorting.cpp
+++ algorithms/sorting/sorting.cpp
@@ -64,6 +64,34 @@ void merge(vector<int> &A, vector<int> &scratch, int left, int middle, int right
     ++merged_i;
   }
 
+  return;
+}
+
+void merge_with_copy_back(vector<int> &A, vector<int> &scratch, int left, int middle, int right) {
+  int first_i = left, second_i = middle, merged_i = left;
+  while (first_i < middle || second_i < right) {
+    if (first_i >= middle) {
+      scratch[merged_i] = A[second_i];
+      ++second_i;
+      ++merged_i;
+      continue;
+    }
+    if (second_i >= right) {
+      scratch[merged_i] = A[first_i];
+      ++first_i;
+      ++merged_i;
+      continue;
+    }
+    if (A[second_i] < A[first_i]) {
+      scratch[merged_i] = A[second_i];
+      ++second_i;
+    } else {
+      scratch[merged_i] = A[first_i];
+      ++first_i;
+    }
+    ++merged_i;
+  }
+
   for (int i = left; i < right; ++i) {
     A[i] = scratch[i];
   }
@@ -71,8 +99,63 @@ void merge(vector<int> &A, vector<int> &scratch, int left, int middle, int right
   return;
 }
 
+void interval_insertion_sort(vector<int> &A, int left, int right) {
+  int j;
+  for (int i = left+1; i < right; ++i) {
+    j = i;
+    while (j > left && A[j] < A[j-1]) {
+      swap(A[j], A[j-1]);
+      --j;
+    }
+  }
+}
+
+void insertion_sort(vector<int> &A) {
+  interval_insertion_sort(A, 0, A.size());
+}
+
+void merge_sort_to_scratch(vector<int> &A, int left, int right, vector<int> &scratch);
+
+void merge_sort_to_A(vector<int> &A, int left, int right, vector<int> &scratch) {
+  if (right-left <= 6) {
+    interval_insertion_sort(A, left, right);
+    return;
+  }
+
+  int middle = left + (right-left)/2;
+  merge_sort_to_scratch(A, left, middle, scratch);
+  merge_sort_to_scratch(A, middle, right, scratch);
+
+  if (A[middle-1] > A[middle]) {
+    merge(scratch, A, left, middle, right);
+  }
+
+  return;
+}
+
+void merge_sort_to_scratch(vector<int> &A, int left, int right, vector<int> &scratch) {
+  if (right-left <= 6) {
+    interval_insertion_sort(A, left, right);
+    for (int i = left; i < right; ++i) {
+      scratch[i] = A[i];
+    }
+    return;
+  }
+
+  int middle = left + (right-left)/2;
+  merge_sort_to_A(A, left, middle, scratch);
+  merge_sort_to_A(A, middle, right, scratch);
+
+  if (A[middle-1] > A[middle]) {
+    merge(A, scratch, left, middle, right);
+  }
+
+  return;
+}
+
 void merge_sort(vector<int> &A, int left, int right, vector<int> &scratch) {
-  if (left+1 >= right) {
+  if (right-left <= 6) {
+    interval_insertion_sort(A, left, right);
     return;
   }
 
@@ -80,7 +163,9 @@ void merge_sort(vector<int> &A, int left, int right, vector<int> &scratch) {
   merge_sort(A, left, middle, scratch);
   merge_sort(A, middle, right, scratch);
 
-  merge(A, scratch, left, middle, right);
+  if (A[middle-1] > A[middle]) {
+    merge_with_copy_back(A, scratch, left, middle, right);
+  }
 
   return;
 }
@@ -88,14 +173,32 @@ void merge_sort(vector<int> &A, int left, int right, vector<int> &scratch) {
 int main() {
   srand(time(NULL));
 
+  // merge sort
+  cout << "Merge sort with copy back\n";
   vector<int> A = generate_random_vector(100);
   vector<int> scratch(A.size());
   print_vector(A);
   merge_sort(A, 0, A.size(), scratch);
   print_vector(A);
-
-
   cout << (is_sorted(A) ? "SUCCESSFULL!" : "WRONG!") << endl;
+
+  // merge sort
+  cout << "Merge sort without copy back\n";
+  vector<int> C = generate_random_vector(100);
+  vector<int> scratch2(C.size());
+  print_vector(C);
+  merge_sort_to_A(C, 0, C.size(), scratch2);
+  print_vector(C);
+  cout << (is_sorted(C) ? "SUCCESSFULL!" : "WRONG!") << endl;
+
+  // insertion sort
+  cout << "Insertion sort\n";
+  vector<int> B = generate_random_vector(100);
+  print_vector(B);
+  insertion_sort(B);
+  print_vector(B);
+  cout << (is_sorted(B) ? "SUCCESSFULL!" : "WRONG!") << endl;
+
 
   return 0;
 }
